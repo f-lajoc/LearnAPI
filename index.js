ce77b20f526ea540459fc04000873fa327ba43a4
lajoc_devs <fisayobolaji7@gmail.com>
2025-03-06 03:25:57 +0100

    boredbot api functionig well and styled

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
-//Api to get random dog breeds
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then(response => response.json())
-  .then(data => {
-     console.log(data);
+// generate Dog breeds on click
+document
+	.getElementById("image-section")
+	.addEventListener("click", function () {
+		//Api to get random dog breeds
+		fetch("https://dog.ceo/api/breeds/image/random")
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
 
- document.getElementById("image-section").innerHTML =
-      `<img src=${data.message} width="50%">`   
-  });
-  
- //Api to get random activity
-fetch("https://apis.scrimba.com/bored/api/activity")
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-  
-    document.getElementById("text-cont").textContent = data.activity
-  }
-)
- //Api to get random bible passage
-fetch("https://bible-api.com/data/web/random")
-	.then((response) => response.json())
-	.then((data) => {
-		console.log(data);
-    document.getElementById("bible-text").textContent =
-      `${data.random_verse.book} ${data.random_verse.chapter}:${data.random_verse.verse} - ${data.random_verse.text}`;
+				document.getElementById(
+					"display"
+				).innerHTML = `<img src=${data.message} width="50%">`;
+			});
 	});
 
+//generate activities on click
+document
+  .getElementById("text-cont")
+  .addEventListener("click", function () {
+    //Api to get random activity
+    fetch("https://apis.scrimba.com/bored/api/activity")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+  
+        document.getElementById(
+					"display"
+				).textContent = `${data.type}: ${data.activity}`;
+      }
+      )
+  })
+
+//generate random bible verses on click
+document.getElementById("bible-text").addEventListener("click", function () {
+	//Api to get random bible passage
+	fetch("https://bible-api.com/data/web/random")
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+			document.getElementById(
+				"display"
+			).textContent = `${data.random_verse.book} ${data.random_verse.chapter}:${data.random_verse.verse} - ${data.random_verse.text}`;
+		});
+});
